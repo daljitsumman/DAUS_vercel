c05e532d844947331d3431db683632af9062c2d5
Daljit Summan <dsumman@gmail.com>
2021-11-01 19:59:04 +0000

    Swapped pages around

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,160 +1,83 @@
-import React, { ReactNode } from 'react';
 import {
-  IconButton,
-  Box,
-  CloseButton,
+  Grid, 
+  GridItem,
+  Box, 
   Flex,
-  Icon,
-  useColorModeValue,
-  Link,
-  Drawer,
-  DrawerContent,
-  Text,
-  useDisclosure,
-  BoxProps,
-  FlexProps,
-} from '@chakra-ui/react';
-import {
-  FiHome,
-  FiTrendingUp,
-  FiCompass,
-  FiStar,
-  FiSettings,
-  FiMenu,
-} from 'react-icons/fi';
-import { IconType } from 'react-icons';
-import { ReactText } from 'react';
+  Input, 
+  Spacer,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  FormHelperText,
+  Button,
+  VStack,
+  StackDivider,
+  Image
+} from "@chakra-ui/react"
 
-interface LinkItemProps {
-  name: string;
-  icon: IconType;
-}
-const LinkItems: Array<LinkItemProps> = [
-  { name: 'Home', icon: FiHome },
-  { name: 'Trending', icon: FiTrendingUp },
-  { name: 'Explore', icon: FiCompass },
-  { name: 'Favourites', icon: FiStar },
-  { name: 'Settings', icon: FiSettings },
-];
+import {FormEventHandler, useState} from "react"
+import { useRouter } from 'next/router';
 
-export default function SimpleSidebar({ children }: { children: ReactNode }) {
-  const { isOpen, onOpen, onClose } = useDisclosure();
-  return (
-    <Box minH="100vh" bg={useColorModeValue('gray.100', 'gray.900')}>
-      <SidebarContent
-        onClose={() => onClose}
-        display={{ base: 'none', md: 'block' }}
-      />
-      <Drawer
-        autoFocus={false}
-        isOpen={isOpen}
-        placement="left"
-        onClose={onClose}
-        returnFocusOnClose={false}
-        onOverlayClick={onClose}
-        size="full">
-        <DrawerContent>
-          <SidebarContent onClose={onClose} />
-        </DrawerContent>
-      </Drawer>
-      {/* mobilenav */}
-      <MobileNav display={{ base: 'flex', md: 'none' }} onOpen={onOpen} />
-      <Box ml={{ base: 0, md: 60 }} p="4">
-        {children}
-      </Box>
-    </Box>
-  );
-}
 
-interface SidebarProps extends BoxProps {
-  onClose: () => void;
-}
 
-const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
-  return (
-    <Box
-      bg={useColorModeValue('white', 'gray.900')}
-      borderRight="1px"
-      borderRightColor={useColorModeValue('gray.200', 'gray.700')}
-      w={{ base: 'full', md: 60 }}
-      pos="fixed"
-      h="full"
-      {...rest}>
-      <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
-        <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
-        </Text>
-        <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
+
+
+export default () => {
+  const [formData, setFormData] = useState({
+      username: "",
+      password: ""
+    });
+    const router = useRouter();
+
+    function onClicked(){
+      if(formData.username ==="admin" && formData.password==="admin"){
+      router.push("/login")
+      }
+  }
+
+return <Grid 
+      h="100vh"
+      templateRows="1fr"
+      templateColumns=".5fr 1fr 1fr .5fr"
+      gap={0}
+      >
+          <GridItem/>
+  <GridItem>
+      <Flex alignItems="center" justifyContent="center" height="100%">
+          <Image src="/Daus_cover_image2.png" height="70vh"/>
       </Flex>
-      {LinkItems.map((link) => (
-        <NavItem key={link.name} icon={link.icon}>
-          {link.name}
-        </NavItem>
-      ))}
-    </Box>
-  );
-};
 
-interface NavItemProps extends FlexProps {
-  icon: IconType;
-  children: ReactText;
-}
-const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
-  return (
-    <Link href="#" style={{ textDecoration: 'none' }}>
-      <Flex
-        align="center"
-        p="4"
-        mx="4"
-        borderRadius="lg"
-        role="group"
-        cursor="pointer"
-        _hover={{
-          bg: 'cyan.400',
-          color: 'white',
-        }}
-        {...rest}>
-        {icon && (
-          <Icon
-            mr="4"
-            fontSize="16"
-            _groupHover={{
-              color: 'white',
-            }}
-            as={icon}
-          />
-        )}
-        {children}
+  </GridItem>    
+  <GridItem>
+      <Flex alignItems="center" justifyContent="center" height="100%">
+              <Box p="4" padding="4em" borderRadius="3px" backgroundColor="white">
+                  <form>
+                  <VStack
+                      divider={<StackDivider border="none"/>}
+                      spacing={4}
+                      align="stretch">
+                      <Box>
+                          <FormControl id="email">
+                          <FormLabel>Email address</FormLabel>
+                          <Input type="text" onChangeCapture={(e) => setFormData({...formData, username: (e.target as HTMLInputElement).value})} value={formData.username}/>
+                          </FormControl>
+                      </Box>
+                      <Box>
+                          <FormControl id="password">
+                          <FormLabel>Password</FormLabel>
+                          <Input type="password" onChangeCapture={(e) => setFormData({...formData, password: (e.target as HTMLInputElement).value})} />
+                          </FormControl>
+                      </Box>
+                      <Box>
+                      <Button onClick={onClicked}>Login</Button>
+                      </Box>
+                  </VStack>
+ 
+                  </form>
+              </Box>
       </Flex>
-    </Link>
-  );
-};
+  </GridItem>    
+  <GridItem/>
+</Grid>
 
-interface MobileProps extends FlexProps {
-  onOpen: () => void;
 }
-const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  return (
-    <Flex
-      ml={{ base: 0, md: 60 }}
-      px={{ base: 4, md: 24 }}
-      height="20"
-      alignItems="center"
-      bg={useColorModeValue('white', 'gray.900')}
-      borderBottomWidth="1px"
-      borderBottomColor={useColorModeValue('gray.200', 'gray.700')}
-      justifyContent="flex-start"
-      {...rest}>
-      <IconButton
-        variant="outline"
-        onClick={onOpen}
-        aria-label="open menu"
-        icon={<FiMenu />}
-      />
-
-      <Text fontSize="2xl" ml="8" fontFamily="monospace" fontWeight="bold">
-        Logo
-      </Text>
-    </Flex>
-  );
-};
