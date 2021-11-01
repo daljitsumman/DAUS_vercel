e6c4bd4db15a045330f699689f0b77ddddc5afb1
Daljit Summan <dsumman@gmail.com>
2021-11-01 19:32:43 +0000

    Login logic completed

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,28 @@
-import { ChakraProvider } from "@chakra-ui/react"
+import { ChakraProvider, extendTheme } from "@chakra-ui/react"
+import "../styles/globals.css"
+const theme = extendTheme({
+  styles: {
+    global: {
+      // styles for the `body`
+      body: {
+        bg: "gray.100",
+        color: "gray.800",
+      },
+      // styles for the `a`
+      a: {
+        color: "teal.500",
+        _hover: {
+          textDecoration: "underline",
+        },
+      },
+    },
+  },
+})
+
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={theme}>
       <Component {...pageProps} />
     </ChakraProvider>
   )
