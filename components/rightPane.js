8fe1ce01b4cd27fe5974031ed88c35baad828e93
Daljit Summan <dsumman@gmail.com>
2022-05-17 21:00:50 +0100

    title for side box

--- components/rightPane.js
+++ components/rightPane.js
@@ -39,14 +39,10 @@ export default function RightPane({ borough, pcn, practise }) {
                     <Col lg="6">
                         <Card style={{ width: '18rem' }}>
                             <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the bulk of
-                                    the card's content.
-                                </Card.Text>
-                                <Card.Link href="#">Card Link</Card.Link>
-                                <Card.Link href="#">Another Link</Card.Link>
+                                <Card.Title>Upcoming shift</Card.Title>
+                                <Card.Title>Booked Shift</Card.Title>
+                                <Card.Title>Cancellation request</Card.Title>
+                                <Card.Title>Swap request</Card.Title>
                             </Card.Body>
                         </Card>
                     </Col>
